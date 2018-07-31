ee83ffb518237ae7e17ae7945f23d4b80882aa75
Peter <pitrens.studio@gmail.com>
2018-08-01 00:08:08 +0800

    setup for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // to tell mongoose which promise library to use
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds259711.mlab.com:59711/andrew-todo-app-node' ||
+mongoose.connect('mongodb://<supertrens>:<Muneca28>@ds259711.mlab.com:59711/andrew-todo-app-node' ||
   'mongodb://localhost:27017/TodoApp'
 );
 

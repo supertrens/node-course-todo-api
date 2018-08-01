864583e313e849504529a197814d0e1fdba8b7ad
Peter <pitrens.studio@gmail.com>
2018-08-01 16:15:42 +0800

    set env variable for mongoLab Url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
+// I set it with this command
+// export MONGOLAB_URI = "mongodb://username:password@ds01316.mlab.com:1316/food"
+
+const remoteUrl = process.env.MONGOLAB_URI;
+
 // to tell mongoose which promise library to use
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<supertrens>:<Muneca28>@ds259711.mlab.com:59711/andrew-todo-app-node' ||
-  'mongodb://localhost:27017/TodoApp'
-);
+mongoose.connect(remoteUrl || 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = { mongoose };

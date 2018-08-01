ac4d7af142d5c7c219ec5114d79520ea724c5528
Peter <pitrens.studio@gmail.com>
2018-08-01 16:39:28 +0800

    inor change in mongoLab url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
-// I set it with this command
 // export MONGOLAB_URI = "mongodb://username:password@ds01316.mlab.com:1316/food"
-
-const remoteUrl = process.env.MONGOLAB_URI;
 
 // to tell mongoose which promise library to use
 mongoose.Promise = global.Promise;
-mongoose.connect(remoteUrl || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI|| 'mongodb://localhost:27017/TodoApp');
 
 
 module.exports = { mongoose };
